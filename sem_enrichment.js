c7e1a42924f5dfdf1b554ee13a665e94f8491f70
lhache <louis.hache@goeuro.com>
2015-12-03 12:09:36 +0000

    empty query string now return empty object

--- sem_enrichment.js
+++ sem_enrichment.js
@@ -2,10 +2,12 @@
 function parseQuery(qstr) {
     var query = {},
     	a = qstr.substr(1).split('&');
-    for (var i = 0, j = a.length; i < j; i++) {
-        var b = a[i].split('=');
-        query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
-    }
+	if (qstr === "") {
+		for (var i = 0, j = a.length; i < j; i++) {
+        	var b = a[i].split('=');
+        	query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
+    	}	
+	}
     return query;
 }
 
